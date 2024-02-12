1eba1b001d28ad20fa244d39b58628c9d54ebb6a
Maherylgrd <maherylgrd@gmail.com>
2024-02-12 15:36:19 +0300

    debug traitementLogin

--- WEB/pages/traitementlogin.php
+++ WEB/pages/traitementlogin.php
@@ -1,12 +1,13 @@
 <?php 
-include 'function.php'; 
+include ('../inc/function.php'); 
 
 $mail=$_POST['mail'];
-$mdp=$_post['mdp'];
+$mdp=$_POST['mdp'];
 $login=login($mail,$mdp);
 
 if($login==-1){
     header("Location:login.php");
+   
 }
 if ($login==-2) {
     header("Location:login.php");
