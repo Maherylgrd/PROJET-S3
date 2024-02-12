bdffe0a73bd0e78d03e0dc0e35e625b8c94ffc2a
MayaSoatiana <razananirinamaya@gmail.com>
2024-02-12 18:16:25 +0300

    add resultat

--- WEB/pages/users/resultat.php
+++ WEB/pages/users/resultat.php
@@ -1,4 +1,54 @@
-<div id="ensemble">
+<div id="ensemble" class="row">
+    <h1>Resultats</h1>
+<div class="col-lg-4">
+        <!-- Yearly Breakup -->
+    <div class="card overflow-hidden">
+        <div class="card-body p-4">
+            <h5 class="card-title mb-9 fw-semibold">Poids Total Cueillettes</h5>
+            <div class="row align-items-center">
+                <div class="col-8">
+                    <h4 class="fw-semibold mb-3">892005 tonnes</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+<div class="col-lg-4">
+        <!-- Yearly Breakup -->
+    <div class="card overflow-hidden">
+        <div class="card-body p-4">
+            <h5 class="card-title mb-9 fw-semibold">Poids restant sur les parcelles</h5>
+            <div class="row align-items-center">
+                <div class="col-8">
+                    <h4 class="fw-semibold mb-3">4554 tonnes</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-4">
+        <!-- Yearly Breakup -->
+    <div class="card overflow-hidden">
+        <div class="card-body p-4">
+            <h5 class="card-title mb-9 fw-semibold">Cout de revient par kg</h5>
+            <div class="row align-items-center">
+                <div class="col-8">
+                    <h4 class="fw-semibold mb-3">28000MGA</h4>
+                </div>
+                <div class="col-4">
+                    <div class="d-flex justify-content-end">
+                        <div
+                        class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
+                        <i class="ti ti-currency-dollar fs-6"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
 
 </div>
