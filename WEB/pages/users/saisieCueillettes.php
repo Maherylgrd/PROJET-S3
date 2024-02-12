1d863814922fc8a8e618df7c42936979e9427230
MayaSoatiana <razananirinamaya@gmail.com>
2024-02-12 17:06:07 +0300

    create user pages

---
+++ WEB/pages/users/saisieCueillettes.php
@@ -0,0 +1,35 @@
+<div id="ensemble">
+    <div id="formulaire">
+        <h2>Saisie des Cueillettes</h2>
+        <form action="admin/traitementCategorieDepenses.php" method="get">
+        
+        <div class="mb-4">
+            <label for="dateCeuilleur" class="form-label">Date Cueillettes</label>
+            <input type="date" class="form-control" id="dateCeuilleur" name="dateCeuilleur">
+        </div>
+
+        <div class="mb-4">
+            <label for="idCueilleur" class="form-label">id Cueilleur</label>
+            <input type="number" class="form-control" id="idCueilleur" name="idCueilleur">
+        </div>
+
+        <div class="mb-4">
+            <label for="idParcelles" class="form-label">id Parcelles</label>
+            <input type="number" class="form-control" id="idParcelles" name="idParcelles">
+        </div>
+
+        <div class="mb-4">
+            <label for="poids" class="form-label">Poids en kg</label>
+            <input type="number" class="form-control" id="poids" name="poids">
+        </div>
+
+
+
+        <div class="mb-4">
+            <input type="submit" class="btn form-control" value="valider">
+        </div>
+            
+        </form>
+    </div>
+
+</div>
---
+++ WEB/pages/users/saisieDepenses.php
@@ -0,0 +1,24 @@
+<div id="ensemble">
+    <div id="formulaire">
+        <h2>Saisie des Depenses</h2>
+        <form action="admin/traitementCategorieDepenses.php" method="get">
+        
+        <div class="mb-4">
+            <label for="idCategorieDepenses" class="form-label">id Categorie Depenses</label>
+            <input type="number" class="form-control" id="idCategorieDepenses" name="idCategorieDepenses">
+        </div>
+
+        <div class="mb-4">
+            <label for="montant" class="form-label">Montant</label>
+            <input type="number" class="form-control" id="montant" name="montant">
+        </div>
+
+
+        <div class="mb-4">
+            <input type="submit" class="btn form-control" value="valider">
+        </div>
+            
+        </form>
+    </div>
+
+</div>
