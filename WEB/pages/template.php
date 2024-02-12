f0d37d4fab10f3d39425299ae62a17e1edfd7dfb
Maherylgrd <maherylgrd@gmail.com>
2024-02-12 16:22:17 +0300

    template miova lien

--- WEB/pages/template.php
+++ WEB/pages/template.php
@@ -51,7 +51,7 @@ session_start();
               <span class="hide-menu">Admin</span>
             </li>
             <li class="sidebar-item">
-              <a class="sidebar-link" href="admin/gestionThe" aria-expanded="false">
+              <a class="sidebar-link" href="admin/gestionThe.php" aria-expanded="false">
                 <span>
                   <i class="ti ti-mood-happy"></i>
                 </span>
@@ -59,7 +59,7 @@ session_start();
               </a>
             </li>
             <li class="sidebar-item">
-              <a class="sidebar-link" href="admin/gestionParcelles" aria-expanded="false">
+              <a class="sidebar-link" href="admin/gestionParcelles.php" aria-expanded="false">
                 <span>
                   <i class="ti ti-aperture"></i>
                 </span>
@@ -67,7 +67,7 @@ session_start();
               </a>
             </li>
             <li class="sidebar-item">
-              <a class="sidebar-link" href="./sample-page.html" aria-expanded="false">
+              <a class="sidebar-link" href="admin/gestionCueilleur.php" aria-expanded="false">
                 <span>
                   <i class="ti ti-aperture"></i>
                 </span>
