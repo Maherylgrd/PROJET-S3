f1720ab7cec98202020a79aea0d1ef35d09a5c7a
MayaSoatiana <razananirinamaya@gmail.com>
2024-02-12 15:48:02 +0300

    creation gestion parcelle

---
+++ WEB/pages/admin/gestionParcelles.php
@@ -0,0 +1,46 @@
+<div id="ensemble">
+    <div id="formulaire">
+        <h2>Insertion Parcelles</h2>
+        <form>
+        <div class="mb-4">
+            <label for="numero" class="form-label">numero parcelle</label>
+            <input type="number" class="form-control" id="numero" name="numero">
+        </div>
+
+        <div class="mb-4">
+            <label for="surface" class="form-label">Surface en hectare</label>
+            <input type="number" class="form-control" id="surface" name="surface">
+        </div>
+
+        <div class="mb-4">
+            <label for="variete" class="form-label">Variete de the plante</label>
+            <input type="text" class="form-control" id="variete" name="variete">
+        </div>
+
+        
+
+        <div class="mb-4">
+            <input type="submit" class="btn form-control" value="valider">
+        </div>
+            
+        </form>
+    </div>
+
+    <div id="tableau">
+        <h2>Tableau Gestion Parcelles</h2>
+        <table class="table table-hover">
+            <tr>
+                <th>Numero parcelle</th>
+                <th>Surface en hectare</th>
+                <th>Variete de the plante</th>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+
+    </div>
+</div>
