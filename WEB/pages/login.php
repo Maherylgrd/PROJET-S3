9ed39dbb602b98f77985ad7cc7b51cdada9625ca
MayaSoatiana <razananirinamaya@gmail.com>
2024-02-12 15:31:04 +0300

    fix error button

--- WEB/pages/login.php
+++ WEB/pages/login.php
@@ -42,10 +42,10 @@
                     </div>
                     <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
                   </div>
-                  <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</a>
+                  <button  type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2" >Sign In</button>
                   <div class="d-flex align-items-center justify-content-center">
                     <p class="fs-4 mb-0 fw-bold">New to BE'leaf?</p>
-                    <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Sign up</a>
+                    <a class="text-primary fw-bold ms-2" href="#">Sign up</a>
                   </div>
                 </form>
               </div>
