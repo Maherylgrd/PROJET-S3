3368f231e60fe0814abc2b41a43337b1774796e0
MayaSoatiana <razananirinamaya@gmail.com>
2024-02-12 21:38:48 +0300

    fix merge conflict

--- WEB/pages/users/resultat.php
+++ WEB/pages/users/resultat.php
@@ -5,7 +5,7 @@ $totalRestantParcelle=calculerPoidTotRestantParcelle();
 ?>
 <div id="ensemble" class="row">
     <h1>Resultats</h1>
-<<<<<<< HEAD
+
 
 <div id="form">
     <form class="form-inline">
@@ -20,23 +20,13 @@ $totalRestantParcelle=calculerPoidTotRestantParcelle();
             </div>
             <div class="form-group col-lg-4 col-md-4 col-sm-8 col-xs-10">
                 <input type="submit" class="form-control" value="chercher" >
-=======
-<div class="col-lg-4">
-        <!-- Yearly Breakup -->
-    <div class="card overflow-hidden">
-        <div class="card-body p-4">
-            <h5 class="card-title mb-9 fw-semibold">Poids Total Cueillettes</h5>
-            <div class="row align-items-center">
-                <div class="col-8">
-                    <h4 class="fw-semibold mb-3"><?php echo $totalPoid ?> tonnes</h4>
-                </div>
->>>>>>> e8564012cf5b07e600a178509482fb6b1297986f
             </div>
-        </div>
+    </div>
     </form>
 </div>
 
-<<<<<<< HEAD
+
+
 <div id="nombres" class="row"> 
     <div class="col-lg-4">
             <!-- Yearly Breakup -->
@@ -46,37 +36,6 @@ $totalRestantParcelle=calculerPoidTotRestantParcelle();
                 <div class="row align-items-center">
                     <div class="col-8">
                         <h4 class="fw-semibold mb-3">892005 tonnes</h4>
-=======
-<div class="col-lg-4">
-        <!-- Yearly Breakup -->
-    <div class="card overflow-hidden">
-        <div class="card-body p-4">
-            <h5 class="card-title mb-9 fw-semibold">Poids restant sur les parcelles</h5>
-            <div class="row align-items-center">
-                <div class="col-8">
-                    <h4 class="fw-semibold mb-3"><?php echo  $totalRestantParcelle?> tonnes</h4>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="col-lg-4">
-        <!-- Yearly Breakup -->
-    <div class="card overflow-hidden">
-        <div class="card-body p-4">
-            <h5 class="card-title mb-9 fw-semibold">Cout de revient par kg</h5>
-            <div class="row align-items-center">
-                <div class="col-8">
-                    <h4 class="fw-semibold mb-3">28000MGA</h4>
-                </div>
-                <div class="col-4">
-                    <div class="d-flex justify-content-end">
-                        <div
-                        class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
-                        <i class="ti ti-currency-dollar fs-6"></i>
-                        </div>
->>>>>>> e8564012cf5b07e600a178509482fb6b1297986f
                     </div>
                 </div>
             </div>
@@ -90,7 +49,7 @@ $totalRestantParcelle=calculerPoidTotRestantParcelle();
                 <h5 class="card-title mb-9 fw-semibold">Poids restant sur les parcelles</h5>
                 <div class="row align-items-center">
                     <div class="col-8">
-                        <h4 class="fw-semibold mb-3">4554 tonnes</h4>
+                        <h4 class="fw-semibold mb-3"><?php echo  $totalRestantParcelle?> tonnes</h4>
                     </div>
                 </div>
             </div>
@@ -115,11 +74,10 @@ $totalRestantParcelle=calculerPoidTotRestantParcelle();
                         </div>
                     </div>
                 </div>
-                
             </div>
         </div>
     </div>
 
-</div>
 
+</div>
 </div>
