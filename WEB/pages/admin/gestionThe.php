ff004a8de6777a86d153d288b224a29cd563854d
MayaSoatiana <razananirinamaya@gmail.com>
2024-02-12 15:41:15 +0300

    create page gestionThe

---
+++ WEB/pages/admin/gestionThe.php
@@ -0,0 +1,44 @@
+<div id="ensemble">
+    <div id="formulaire">
+        <h2>Insertion Variation de Thé</h2>
+        <form>
+        <div class="mb-4">
+            <label for="variete" class="form-label">Nom variete de the</label>
+            <input type="text" class="form-control" id="variete" name="variete">
+        </div>
+
+        <div class="mb-4">
+            <label for="occupation" class="form-label">Occupation</label>
+            <input type="text" class="form-control" id="occupation" name="occupation">
+        </div>
+
+        <div class="mb-4">
+            <label for="rendement" class="form-label">Rendement par pied</label>
+            <input type="number" class="form-control" id="rendement" name="rendement">
+        </div>
+
+        <div class="mb-4">
+            <input type="submit" class="btn form-control" value="valider">
+        </div>
+            
+        </form>
+    </div>
+
+    <div id="tableau">
+        <h2>Tableau Gestion Variete de The</h2>
+        <table class="table table-hover">
+            <tr>
+                <th>Nom variete the</th>
+                <th>Occupation</th>
+                <th>Rendement par pied</th>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+
+    </div>
+</div>
