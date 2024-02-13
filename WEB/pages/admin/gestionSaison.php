f92c8eaedc43f5a016b8bb512633042ea6e401ab
Maherylgrd <maherylgrd@gmail.com>
2024-02-13 10:20:42 +0300

    saison

--- WEB/pages/admin/gestionSaison.php
+++ WEB/pages/admin/gestionSaison.php
@@ -1,55 +1,27 @@
 
+<?php
+$tabMois = array(
+    "janvier", "fevrier", "mars", "avril", "mai", "juin",
+    "juillet", "aout", "septembre", "octobre", "novembre", "decembre"
+);
+?>
+?>
 <div id="ensemble">
     <div id="formulaire">
         <h2>Insertion Saison</h2>
-        <form action="admin/traitementCueilleur.php" method="get">
-        <div class="d-flex mb-4">
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 1</label>
-            </div>
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 2</label>
-            </div>
-        </div>
-        <div class=" d-flex mb-4">    
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 3</label>
-            </div>
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 4</label>
-            </div>
-        </div>
-        <div class=" d-flex mb-4">    
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 3</label>
-            </div>
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 4</label>
-            </div>
-        </div>
-        <div class=" d-flex mb-4">    
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 3</label>
-            </div>
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 4</label>
-            </div>
-        </div>
-        <div class=" d-flex mb-4">    
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 3</label>
-            </div>
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 4</label>
-            </div>
-        </div>
-        <div class=" d-flex mb-4">    
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 3</label>
-            </div>
-            <div class="col-md-6">
-                <input type="checkbox" name=""> <label > Check 4</label>
-            </div>
+        <form action="admin/traitementCueilleur.php?<?php echo http_build_query(['tabmois' => $tabMois]) ?>" method="get">
+        <div class=" col-md-12 mb-4">
+        <?php 
+            for ($i=0; $i <count($tabMois) ; $i++) { 
+                ?>
+                
+                    <div class="col-md-3">
+                        <input type="checkbox" name="<?php echo $tabMois[$i] ?>"> <label > <?php echo $tabMois[$i] ?></label>
+                    </div>
+                    
+                
+        <?php    }
+        ?>
         </div>
         <div class="  mt-2 ">
             <button type="submit" class="btn btn-primary w-25">Valider</button>
