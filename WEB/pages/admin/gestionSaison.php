6037c97c8115bdb8c9af5834b939b6d4614a5597
Maherylgrd <maherylgrd@gmail.com>
2024-02-13 09:59:29 +0300

    saison

---
+++ WEB/pages/admin/gestionSaison.php
@@ -0,0 +1,62 @@
+
+<div id="ensemble">
+    <div id="formulaire">
+        <h2>Insertion Saison</h2>
+        <form action="admin/traitementCueilleur.php" method="get">
+        <div class="d-flex mb-4">
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 1</label>
+            </div>
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 2</label>
+            </div>
+        </div>
+        <div class=" d-flex mb-4">    
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 3</label>
+            </div>
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 4</label>
+            </div>
+        </div>
+        <div class=" d-flex mb-4">    
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 3</label>
+            </div>
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 4</label>
+            </div>
+        </div>
+        <div class=" d-flex mb-4">    
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 3</label>
+            </div>
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 4</label>
+            </div>
+        </div>
+        <div class=" d-flex mb-4">    
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 3</label>
+            </div>
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 4</label>
+            </div>
+        </div>
+        <div class=" d-flex mb-4">    
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 3</label>
+            </div>
+            <div class="col-md-6">
+                <input type="checkbox" name=""> <label > Check 4</label>
+            </div>
+        </div>
+        <div class="  mt-2 ">
+            <button type="submit" class="btn btn-primary w-25">Valider</button>
+        </div>
+                      
+    </div>
+    </form>
+</div>
+
+</div>
